c12f8bc53e2aea5772d7c609c9c88b834918ccb0
Connor McKenna <cmckenna@adap.tv>
2013-04-01 16:37:47 -0700

    improved settings activity
    make passcode preference display the
    current passcode

--- src/com/foggyciti/macremote/PersistenceHandler.java
+++ src/com/foggyciti/macremote/PersistenceHandler.java
@@ -1,29 +1,32 @@
 package com.foggyciti.macremote;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 public class PersistenceHandler {
-	private static final String PREFS_NAME="DefaultPrefs";
 	
-	private static final String passcodeKey = "@string/passcodeKey";
-
-	private static SharedPreferences.Editor getEditor(Context c, String name) {
-		return c.getSharedPreferences(name, Context.MODE_PRIVATE).edit();
+	private static SharedPreferences.Editor getEditor(Context c) {
+		return getPrefs(c).edit();
 	}
 	
-	private static SharedPreferences getPrefs(Context c, String name) {
-		return c.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+	private static SharedPreferences getPrefs(Context c) {
+		return PreferenceManager.getDefaultSharedPreferences(c);
+	}
+	
+	public static String getPasscodeKey(Context c) {
+		return c.getResources().getString(R.string.passcodeKey);
 	}
 	
 	public static void savePasscode(Context c, String passcode) {
-		SharedPreferences.Editor editor = getEditor(c, PREFS_NAME);
-		editor.putString(passcodeKey, passcode);
+		SharedPreferences.Editor editor = getEditor(c);
+		editor.putString(getPasscodeKey(c), passcode);
 		editor.commit();
 	}
 	
 	public static String getPasscode(Context c) {
-		SharedPreferences prefs = getPrefs(c, PREFS_NAME);
-		return prefs.getString(passcodeKey, null);
+		SharedPreferences prefs = getPrefs(c);
+		return prefs.getString(getPasscodeKey(c), null);
 	}
 }
--- src/com/foggyciti/macremote/SettingsActivity.java
+++ src/com/foggyciti/macremote/SettingsActivity.java
@@ -1,7 +1,11 @@
 package com.foggyciti.macremote;
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.text.Selection;
+import android.widget.EditText;
 
 public class SettingsActivity extends PreferenceActivity {
 	
@@ -9,5 +13,15 @@ public class SettingsActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		EditTextPreference passcodePreference = (EditTextPreference)findPreference(PersistenceHandler.getPasscodeKey(this));
+		passcodePreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				EditText editText = ((EditTextPreference)preference).getEditText();
+				editText.setText(PersistenceHandler.getPasscode(SettingsActivity.this));
+				Selection.setSelection(editText.getText(), editText.length());
+				return false;
+			}
+		});
 	}
 }
